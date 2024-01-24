78ef979a77e6d5814a748d299b68b345e6a30927
Hashid <hashidlouis@gmail.com>
2024-01-24 10:22:14 -0500

    updated products

--- src/products.js
+++ src/products.js
@@ -1,9 +1,9 @@
 // products.js
 const products = [
-    { id: 1, name: 'Product Nike', price: 50 },
-    { id: 2, name: 'Product Jordan', price: 250 },
-    { id: 3, name: 'Product Adidas', price: 150 },
-    { id: 4, name: 'Product Fendi', price: 300 },
+    { id: 1, name: 'Nike', price: 50 },
+    { id: 2, name: 'Jordan', price: 250 },
+    { id: 3, name: 'Adidas', price: 150 },
+    { id: 4, name: 'Fendi', price: 300 },
     // Add more products as needed
   ];
   
